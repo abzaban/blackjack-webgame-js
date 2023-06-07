04bf5d30a21291f825f717be5aabab0fed74bc01
abzaban <abrahamberrelleza10@outlook.com>
2023-06-07 11:05:04 -0700

    finished --stable

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,43 +10,6 @@ const initDeck = () => {
             deck.push(`${num}${type}`)
 
     deck = _.shuffle(deck);
-    console.log(deck)
-}
-
-const askCard = () => {
-    playerHand.push(deck.pop());
-    console.log(playerHand[playerHand.length - 1]);
-
-    let hand = document.getElementsByClassName('cards-container')[0];
-    hand.innerHTML += `<img src="assets/img/${playerHand[playerHand.length - 1]}.png" width="100vw">`;
-
-    if (handIs21(playerHand)) {
-        let askBtn = document.getElementById('ask-btn');
-        askBtn.disabled = true
-        askBtn.classList.add('disabled');
-        pcTurn();
-        return;
-    }
-}
-
-const handIs21 = (hand) => {
-    let total = 0;
-    for (card of hand) {
-        let letter = card.charAt(0)
-        total += (letter === 'A') ? 1 : (letter === 'J') ? 11 : (letter === 'Q') ? 12 : (letter.charAt(0) === 'K') ? 13 : parseInt(letter);
-    }
-
-    return total >= 21;
-}
-
-const pcTurn = () => {
-    while (!handIs21(computerHand)) {
-        computerHand.push(deck.pop());
-        console.log(computerHand[computerHand.length - 1]);
-
-        let hand = document.getElementsByClassName('cards-container')[1];
-        hand.innerHTML += `<img src="assets/img/${computerHand[computerHand.length - 1]}.png" width="100vw">`;
-    }
 }
 
 const newGame = () => {
@@ -57,16 +20,75 @@ const newGame = () => {
     for (hand of hands)
         hand.innerHTML = '';
 
-    let askBtn = document.getElementById('ask-btn');
-    askBtn.disabled = false;
-    askBtn.classList.remove('disabled');
+    disableBtn('ask-btn', false);
+    disableBtn('stop-btn', false);
 
     playerHand = [];
     computerHand = [];
 }
 
-const stopGame = () => {
+const askCard = async () => {
+    playerHand.push(deck.pop());
 
+    let hand = document.getElementsByClassName('cards-container')[0];
+    hand.innerHTML += `<img src="assets/img/${playerHand[playerHand.length - 1]}.png" width="100vw">`;
+
+    if (calculateTotal(playerHand) > 21) {
+        disableBtn('ask-btn', true);
+        disableBtn('stop-btn', true);
+        pcTurn();
+        setTimeout(() => alert('Game Over'), 100);
+        return;
+    }
+}
+
+const stopGame = () => {
+    let totalComputer = 0;
+    let totalPlayer = calculateTotal(playerHand);
+    do {
+        pcTurn();
+        totalComputer = calculateTotal(computerHand);
+    } while (totalComputer < totalPlayer);
+
+    checkWinner(totalPlayer, totalComputer);
+
+    disableBtn('ask-btn', true);
+    disableBtn('stop-btn', true);
+}
+
+const calculateTotal = (hand) => {
+    let total = 0;
+    for (card of hand) {
+        let letter = card.charAt(0)
+        total += (letter === 'A') ? 1 : (letter === 'J') ? 11 : (letter === 'Q') ? 12 : (letter === 'K') ? 13 : (card.slice(0, 2) === '10') ? 10 : parseInt(letter);
+    }
+
+    return total;
+}
+
+const pcTurn = () => {
+    computerHand.push(deck.pop());
+
+    let hand = document.getElementsByClassName('cards-container')[1];
+    hand.innerHTML += `<img src="assets/img/${computerHand[computerHand.length - 1]}.png" width="100vw">`;
+}
+
+const checkWinner = (totalPlayer, totalComputer) => {
+    if (totalComputer > totalPlayer && totalComputer <= 21)
+        setTimeout(() => alert('Computer wins!!!'), 100)
+    else if (totalComputer === totalPlayer)
+        setTimeout(() => alert('Draw!!!'), 100);
+    else
+        setTimeout(() => alert('Player wins!!!'), 100);
+}
+
+const disableBtn = (btnId, disable) => {
+    let btn = document.getElementById(btnId);
+    btn.disabled = disable;
+    if (disable)
+        btn.classList.add('disabled');
+    else
+        btn.classList.remove('disabled');
 }
 
 initDeck();
